19d9d2cd952539c46be771d6a1de4e0a32aa0125
Bibek <bibek.joshi91@gmail.com>
2024-04-04 16:46:27 +0545

    first commit

---
+++ Install.py
@@ -0,0 +1,51 @@
+
+
+# How to install Python and Django
+
+# Steps
+
+1. Install Python from google is not installed in your Computer.
+
+2. Make a project folder and open in cmd prompt/powershell by hitting cmd in location bar.
+   
+    path> cmd
+
+3. Install Virtual Environment 
+
+    path> pip install virtualenv
+
+4. Start Virtual EnvironmentError
+
+    path> virtualenv env
+
+5. Activate Virtual Environment
+    path> cd env/
+    path\env> cd Scripts
+    path\env\Scripts> activate
+    path\env\Scripts> cd ..
+    path\env> cd ..
+
+
+6. Install Django
+
+    path> pip install Django
+
+Django is installed
+
+
+7. Start a new project named "core"
+
+    path> django-admin startproject core
+
+8. VS Code
+
+    path> code .
+
+#you can see folder named core and venv
+
+
+
+
+
+
+
